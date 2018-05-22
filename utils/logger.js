2bef70d4b63c13df3d587444b32ac17eb98af149
NullDev (Shadow) <NL-Dev@yandex.com>
2018-05-22 15:41:31 +0200

    logger

---
+++ utils/logger.js
@@ -0,0 +1,24 @@
+"use strict";
+
+////////////////////////////////
+//----------------------------//
+// Copyright (c) 2018 NullDev //
+//----------------------------//
+////////////////////////////////
+
+let log = function(txt, err){
+    const date = new Date();
+    let hour = date.getHours(),
+        min  = date.getMinutes(),
+        sec  = date.getSeconds();
+
+    hour  = (hour < 10 ? "0" : "") + hour;
+    min   = (min  < 10 ? "0" : "") + min;
+    sec   = (sec  < 10 ? "0" : "") + sec;
+
+    let head = (err ? "[ERROR]" : "[INFO] ");
+
+    console.log(head + " [" + hour + ":" + min + ":" + sec + "] - " + txt);
+};
+
+module.exports = log;
