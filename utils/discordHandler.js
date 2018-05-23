64953cedaca958739034fb531a937c3c65c1a4a6
NullDev (Shadow) <NL-Dev@yandex.com>
2018-05-23 10:56:52 +0200

    added main function

--- utils/discordHandler.js
+++ utils/discordHandler.js
@@ -8,23 +8,35 @@
 
 let Discord = require("discord.js");
 
+let log = require("./logger");
+
 let isset = function(obj){ return !!(obj && obj !== null && (typeof obj === "string" && obj !== "")); }
 
-log("Logging in...");
-
-if (!isset(token)){
-    log("No Token Provided. Exiting...", true);
-    process.exit(1);
-}
-
-var client = new class Client extends Discord.Client {
-    constructor() {
-        super({ fetchAllMembers: true });
-        this.on("ready", () => { 
-            log(`Logged in as ${client.user.tag}`);
-            this.user.setActivity("testing my selfbot...");
-            log("Started."); 
-        });
-        this.login(token);
+let updateStatus = function(token, text, callback){
+    log("Logging in...", false, true);
+    if (!isset(token)){
+        log("No Token Provided.", true, true);
+        callback("no token");
     }
+
+    let client = new class Client extends Discord.Client {
+        constructor() {
+            super({ fetchAllMembers: true });
+            this.on("ready", () => { 
+                log(`Logged in as ${client.user.tag}`, false, true);
+                this.user.setActivity(text);
+                log("Status updated.", false, true); 
+                callback();
+            });
+            this.on("error", () => {
+                log("Invalid Token Provided.", true, true);
+                callback("invalid token");
+            });
+            this.login(token);
+        }
+    };
 };
+
+process.on("unhandledRejection", err => log(`Uncaught Promise Rejection:\n${err}`, true, true));
+
+module.exports = updateStatus;
