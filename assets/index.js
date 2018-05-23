7dfb21b170919780f49c1746ea5970e296c90f8b
NullDev (Shadow) <NL-Dev@yandex.com>
2018-05-23 10:57:08 +0200

    fixed and added stuff

--- assets/index.js
+++ assets/index.js
@@ -6,16 +6,31 @@
 //----------------------------//
 ////////////////////////////////
 
-let shell = require("electron").shell;
-let electron = require("electron");
+let shell         = require("electron").shell;
+let electron      = require("electron");
 let BrowserWindow = electron.remote.BrowserWindow;
-let remote = require("electron").remote;
+let remote        = require("electron").remote;
+
+let log          = require(__dirname + "/../utils/logger");
+let updateStatus = require(__dirname + "/../utils/discordHandler");
+let Store        = require(__dirname + "/../utils/appData");
+
+const store = new Store();
 
 window.$ = window.jQuery = require("jquery");
+
+let isset = function(obj){ return !!(obj && obj !== null && (typeof obj === "string" && obj !== "")); }
 
 let openUri = function(url){ shell.openExternal(url); };
 
 $(document).ready(function(){
+
+    let injectToken  = store.get("token");
+    let injectStatus = store.get("status");
+
+    if (isset(injectToken))  $("#token").val(injectToken);
+    if (isset(injectStatus)) $("#status").val(injectStatus);
+
     $("#t-help").on("click", function(e){
         let top = remote.getCurrentWindow();
 
@@ -41,7 +56,32 @@ $(document).ready(function(){
         child.once("ready-to-show", () => { child.show(); });
     });
 
-    $("login-link").on("click", function(e){ openUri("https://discordapp.com/login"); });
+    $("#submit").on("click", function(e){
+        let err = [];
 
-    $("help-credit").on("click", function(e){ openUri("https://github.com/TheRacingLion/Discord-SelfBot/wiki/Discord-Token-Tutorial"); });
+        let utoken = $("#token").val();
+        let status = $("#status").val();
+
+        if (!isset(utoken)) err.push("Token is not set.");
+        if (!isset(status)) err.push("Status text is empty.");
+
+        if (err.length >= 1){
+            let errtxt = "Please correct the following errors:\n\n";
+            for (let i in err) errtxt += "- " + err[i] + "\n";
+            return alert(errtxt);
+        }
+
+        updateStatus(utoken, status, function(err){
+            if (err) return alert("Error: " + err);
+
+            store.set("token", utoken);
+            store.set("status", status);
+
+            alert("Status updated!");
+        });
+    });
+
+    $("#login-link").on("click", function(e){ openUri("https://discordapp.com/login"); });
+
+    $("#help-credit").on("click", function(e){ openUri("https://github.com/TheRacingLion/Discord-SelfBot/wiki/Discord-Token-Tutorial"); });
 });
